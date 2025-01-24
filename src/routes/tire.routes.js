b12265a7edfb7dbc84d81101023a14ef91ac3bb1
Patricio pittana <pittanapatricio@gmail.com>
2025-01-24 20:44:29 -0300

    add validation middleware for tire routes to ensure tire exists before fetching or updating

--- src/routes/tire.routes.js
+++ src/routes/tire.routes.js
@@ -1,11 +1,12 @@
 import express from 'express';
 import TireController from '../controller/tire.controller.js';
+import { validateTireExists } from '../middleware/tireExists.js';
 
 const router = express.Router();
 
 router.get('/', TireController.getAll);
-router.get('/:id', TireController.getById);
-router.put('/:id', TireController.update);
+router.get('/:id', validateTireExists, TireController.getById);
+router.put('/:id', validateTireExists, TireController.update);
 router.post('/', TireController.create);
 
 export default router;
